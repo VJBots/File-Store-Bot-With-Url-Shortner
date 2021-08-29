3f8e3918f68bc454e1b1875eca92dcfde9af6893
Predator HackerzZ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-08-29 23:04:58 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -1,4 +1,4 @@
-# (c) Mr. Vishal & @AbirHasan2005
+# (c) Mr. Vishal & @AbirHasan2005 @PredatorHackerzZ
 
 import datetime
 from configs import Config
@@ -23,6 +23,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned to Use This Bot 🥺", quote=True)
+            await cmd.reply_text("𝐘𝐨𝐮 𝐚𝐫𝐞 𝐁𝐚𝐧𝐧𝐞𝐝 𝐭𝐨 𝐔𝐬𝐞 𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 😝", quote=True)
             return
     await cmd.continue_propagation()
