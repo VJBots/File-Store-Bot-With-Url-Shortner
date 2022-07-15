c7a41501f7b43e98658b73d9ec7e244b5da604b7
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-07-15 23:08:07 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -8,19 +8,19 @@ from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
 
-async def ReplyForward(message: Message, file_id: int):
+async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"🔸 **Here is Shareable Link Of You Given Media:**\n"
-            f"🔹 https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(file_id))}\n\n"
-            f"🔸 __To Get the Stored Media, just open the given link!__",
+            f"**Here is Sharable Link of this file:**\n"
+            f"https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(file_id))}\n\n"
+            f"__To Retrive the Stored File, just open the link!__",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
-        await asyncio.sleep(e.x)
-        await ReplyForward(message, file_id)
+        await asyncio.sleep(e.value)
+        await reply_forward(message, file_id)
 
 
-async def MediaForward(bot: Client, user_id: int, file_id: int):
+async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
@@ -29,11 +29,11 @@ async def MediaForward(bot: Client, user_id: int, file_id: int):
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return MediaForward(bot, user_id, file_id)
+        await asyncio.sleep(e.value)
+        return media_forward(bot, user_id, file_id)
 
 
-async def SendMediaAndReply(bot: Client, user_id: int, file_id: int):
-    sent_message = await MediaForward(bot, user_id, file_id)
-    await ReplyForward(message=sent_message, file_id=file_id)
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+    sent_message = await media_forward(bot, user_id, file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(2)
