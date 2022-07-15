5f61eefdc1032f8da9eff787286414acdeb3833e
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-07-15 23:00:54 +0530

    Update force_sub_handler.py
    😵

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -1,55 +1,68 @@
 # (c) @PredatorHackerzZ
 
 import asyncio
+from typing import (
+    Union
+)
 from configs import Config
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 
-async def handle_force_sub(bot: Client, cmd: Message):
+async def get_invite_link(bot: Client, chat_id: Union[str, int]):
     try:
-        user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cmd.from_user.id)
-        if user.status == "banned":
+        invite_link = await bot.create_chat_invite_link(chat_id=chat_id)
+        return invite_link
+    except FloodWait as e:
+        print(f"Sleep of {e.value}s caused by FloodWait ...")
+        await asyncio.sleep(e.value)
+        return await get_invite_link(bot, chat_id)
+
+
+async def handle_force_sub(bot: Client, cmd: Message):
+    if Config.UPDATES_CHANNEL and Config.UPDATES_CHANNEL.startswith("-100"):
+        channel_chat_id = int(Config.UPDATES_CHANNEL)
+    elif Config.UPDATES_CHANNEL and (not Config.UPDATES_CHANNEL.startswith("-100")):
+        channel_chat_id = Config.UPDATES_CHANNEL
+    else:
+        return 200
+    try:
+        user = await bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
+        if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="You are Banned to use Me. Contact my [Support Group](https://t.me/TeleRoid14).",
-                parse_mode="markdown",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/TeleRoid14).",
                 disable_web_page_preview=True
             )
             return 400
     except UserNotParticipant:
         try:
-            invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
+            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
         except Exception as err:
             print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
             return 200
         await bot.send_message(
             chat_id=cmd.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**\n\n"
-                 "Due to Overland Use Only Channel Subscribers can Use this Bot!",
+                 "Due to Overload, Only Channel Subscribers can use this Bot!",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("⭕ Join Updates Channel ⭕", url=invite_link.invite_link)
+                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
                     ],
                     [
                         InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshForceSub")
                     ]
                 ]
-            ),
-            parse_mode="markdown"
+            )
         )
         return 400
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went wrong. Contact Here [Support Group](https://t.me/TeleRoid14).",
-            parse_mode="markdown",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/TeleRoid14).",
             disable_web_page_preview=True
         )
-        return 400
+        return 200
     return 200
