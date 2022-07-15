2090b101215689eec11ffe04fc17382448636d52
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-07-15 23:06:25 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -3,37 +3,40 @@
 import asyncio
 from configs import Config
 from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
 
-async def ForwardToChannel(bot: Client, message: Message, editable: Message):
+async def forward_to_channel(bot: Client, message: Message, editable: Message):
     try:
         __SENT = await message.forward(Config.DB_CHANNEL)
         return __SENT
     except FloodWait as sl:
-        if sl.x > 45:
-            await asyncio.sleep(sl.x)
+        if sl.value > 45:
+            await asyncio.sleep(sl.value)
             await bot.send_message(
                 chat_id=int(Config.LOG_CHANNEL),
-                text=f"#FloodWait:\nGot FloodWait of `{str(sl.x)}s` from `{str(editable.chat.id)}` !!",
-                parse_mode="Markdown",
+                text=f"#FloodWait:\nGot FloodWait of `{str(sl.value)}s` from `{str(editable.chat.id)}` !!",
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("👤𝐁𝐚𝐧 𝐔𝐬𝐞𝐫", callback_data=f"ban_user_{str(editable.chat.id)}")]
+                        [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
                     ]
                 )
             )
-        return await ForwardToChannel(bot, message, editable)
+        return await forward_to_channel(bot, message, editable)
 
 
-async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: list):
+async def save_batch_media_in_channel(bot: Client, editable: Message, message_ids: list):
     try:
         message_ids_str = ""
         for message in (await bot.get_messages(chat_id=editable.chat.id, message_ids=message_ids)):
-            sent_message = await ForwardToChannel(bot, message, editable)
+            sent_message = await forward_to_channel(bot, message, editable)
             if sent_message is None:
                 continue
             message_ids_str += f"{str(sent_message.message_id)} "
@@ -43,18 +46,17 @@ async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: l
             text=message_ids_str,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("𝐃𝐞𝐥𝐞𝐭𝐞 𝐁𝐚𝐭𝐜𝐡", callback_data="closeMessage")
+                InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
         share_link = f"https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(SaveMessage.message_id))}"
         await editable.edit(
-            f"**Batch files Added in Database!**\n\nHere is the Permanent Link Of Your Media: {share_link} \n\n"
-            f"Just Click On The Given Shareable Link!",
-            parse_mode="Markdown",
+            f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
+            f"Just Click the link to get your files!",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("⭕ Channel ⭕", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("⭕ Support ⭕", url="https://t.me/TeleRoid14")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
+                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
             ),
             disable_web_page_preview=True
         )
@@ -62,67 +64,68 @@ async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: l
             chat_id=int(Config.LOG_CHANNEL),
             text=f"#BATCH_SAVE:\n\n[{editable.reply_to_message.from_user.first_name}](tg://user?id={editable.reply_to_message.from_user.id}) Got Batch Link!",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝐎𝐩𝐞𝐧 𝐋𝐢𝐧𝐤", url=share_link)]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=share_link)]])
         )
     except Exception as err:
         await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
         await bot.send_message(
             chat_id=int(Config.LOG_CHANNEL),
             text=f"#ERROR_TRACEBACK:\nGot Error from `{str(editable.chat.id)}` !!\n\n**Traceback:** `{err}`",
-            parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("👤𝐁𝐚𝐧 𝐔𝐬𝐞𝐫", callback_data=f"ban_user_{str(editable.chat.id)}")]
+                    [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
                 ]
             )
         )
 
 
-async def SaveMediaInChannel(bot: Client, editable: Message, message: Message):
+async def save_media_in_channel(bot: Client, editable: Message, message: Message):
     try:
         forwarded_msg = await message.forward(Config.DB_CHANNEL)
         file_er_id = str(forwarded_msg.message_id)
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
-            parse_mode="Markdown", disable_web_page_preview=True)
+            disable_web_page_preview=True)
         share_link = f"https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(file_er_id)}"
         await editable.edit(
-            f"**Your Media Stored in My Database!**\n\nHere is the Permanent Shareable Link Of Your Files: {share_link} \n\n"
-            f"Just Click On the Link to Reterive the Files!",
-            parse_mode="Markdown",
+            "**Your File Stored in my Database!**\n\n"
+            f"Here is the Permanent Link of your file: {share_link} \n\n"
+            "Just Click the link to get your file!",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("⭕ Channel ⭕", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("⭕ Support ⭕", url="https://t.me/TeleRoid14")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
+                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
             ),
             disable_web_page_preview=True
         )
     except FloodWait as sl:
-        if sl.x > 45:
-            await asyncio.sleep(sl.x)
+        if sl.value > 45:
+            print(f"Sleep of {sl.value}s caused by FloodWait ...")
+            await asyncio.sleep(sl.value)
             await bot.send_message(
                 chat_id=int(Config.LOG_CHANNEL),
-                text=f"#FloodWait:\nGot FloodWait of `{str(sl.x)}s` from `{str(editable.chat.id)}` !!",
-                parse_mode="Markdown",
+                text="#FloodWait:\n"
+                     f"Got FloodWait of `{str(sl.value)}s` from `{str(editable.chat.id)}` !!",
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("👤𝐁𝐚𝐧 𝐔𝐬𝐞𝐫", callback_data=f"ban_user_{str(editable.chat.id)}")]
+                        [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
                     ]
                 )
             )
-        await SaveMediaInChannel(bot, editable, message)
+        await save_media_in_channel(bot, editable, message)
     except Exception as err:
         await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
         await bot.send_message(
             chat_id=int(Config.LOG_CHANNEL),
-            text=f"#ERROR_TRACEBACK:\nGot Error from `{str(editable.chat.id)}` !!\n\n**Traceback:** `{err}`",
-            parse_mode="Markdown",
+            text="#ERROR_TRACEBACK:\n"
+                 f"Got Error from `{str(editable.chat.id)}` !!\n\n"
+                 f"**Traceback:** `{err}`",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("👤𝐁𝐚𝐧 𝐔𝐬𝐞𝐫", callback_data=f"ban_user_{str(editable.chat.id)}")]
+                    [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
                 ]
             )
         )
