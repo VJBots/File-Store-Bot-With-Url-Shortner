505d84c1d9e12d29e2b4d1ccb7d225f41b6314b3
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-07-15 23:40:17 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,15 +2,16 @@
 
 import os
 
+
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))
+	API_ID = int(os.environ.get("API_ID", "0"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
