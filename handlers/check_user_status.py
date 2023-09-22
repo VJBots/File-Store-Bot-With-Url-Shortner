9ba1f34ba8bf4bbfb7944582e69cd4fb1786f40c
Uwiejsle <115135419+Mokshb6458@users.noreply.github.com>
2023-09-22 20:56:23 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -23,6 +23,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @TeleRoid14 😝", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact @CF_Support_Group 😝", quote=True)
             return
     await cmd.continue_propagation()
