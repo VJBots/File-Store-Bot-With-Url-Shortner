ac7d4938ebfa6f5026a3984ca70eab107ea0c36e
Uwiejsle <115135419+Mokshb6458@users.noreply.github.com>
2023-09-22 18:58:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,59 +3,44 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
-	SHORTLINK_API = os.environ.get('SHORTLINK_API')
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	ABOUT_BOT_TEXT = f"""
+  API_ID = int(os.environ.get("API_ID", "9529563"))
+  API_HASH = os.environ.get("API_HASH", "a6c1e1c8daecd5d1f7da03d42032d09a")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6561581223:AAGcf-P8kfymZ0sXZktph3sbjmVMNR6R2rA")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "CF_Study_Bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001580035304"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "GreyMatterslinks.in")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "20eb8456008878c0349fc79d40fb4d1634cccf12")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "5151412494"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://GreyMatter64616:GreyMatter24410062@cluster0.lgnzq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001682019198")
+  LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001737875604")
+  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
 
-╭────[ **🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅**]────⍟
+╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 **My Name:** [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
 │
-├🔸 **Language:** [Python 3](https://www.python.org)
+├🔸 Language: [Python 3](https://www.python.org)
 │
-├🔹 **Library:** [Pyrogram](https://docs.pyrogram.org)
-│
-├🔹 **Hosted On:** [Heroku](https://heroku.com)
-│
-├🔸 **Developer:** [Predator HackerzZ](https://t.me/OwnYourBotz) 
-│
-├🔹 **Bot Support:** [Support Group](https://t.me/TeleRoid14)
-│
-├🔸 **Bot Updates:** [Bots Channel](https://t.me/TeleRoidGroup)
+├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
 │
 ╰──────[ 😎 ]───────────⍟
 """
-	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@PredatorHackerzZ](https://github.com/PredatorHackerzZ)
+  ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [@PredatorHackerzZ](https://github.com/PredatorHackerzZ)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/DonateXrobot) or ```MrAbhi2k3@apl```
+[Donate Me](https://t.me/DonateXrobot) or MrAbhi2k3@apl
 """
-	HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+  HOME_TEXT = """
+Hello, [{}](tg://user?id={})\n\nThis is a Permanent FileStore Bot.
 
-How to Use Bot & it's Benefits??
-
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
-
-❌ **PORNOGRAPHY CONTENTS** are strictly prohibited & get Permanent Ban.
-"""
+Join: @Cinematic_Fusion"""
