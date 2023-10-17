1f838d2ca2cb2caef4b9c9ec9d80ee4d05c74b17
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-17 07:59:47 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -1,5 +1,3 @@
-# (c) Mr. Vishal & @AbirHasan2005 @PredatorHackerzZ
-
 import datetime
 from configs import Config
 from handlers.database import Database
@@ -23,6 +21,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @GreyMatter_Support 😝", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact @VJ_Botz 😝", quote=True)
             return
     await cmd.continue_propagation()
