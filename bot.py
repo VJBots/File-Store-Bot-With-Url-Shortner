fc195c07f133dde8e7f9d21eae9ce2fc334b523d
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-09-24 10:38:19 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -354,7 +354,7 @@ async def button(bot: Client, cmd: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/VJ_Botz")
+                        InlineKeyboardButton("Updates Channel", url="https://t.me/latest_tv_Show")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
