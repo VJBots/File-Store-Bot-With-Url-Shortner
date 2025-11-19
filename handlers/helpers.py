f06f8bc4640675a135e82549bfdf8e823665b6e5
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-11-19 13:42:49 +0530

    Update helpers.py

--- handlers/helpers.py
+++ handlers/helpers.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from base64 import standard_b64encode, standard_b64decode
 
 
